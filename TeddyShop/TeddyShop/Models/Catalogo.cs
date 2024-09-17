cc6d897a2ab362ee297b5657013619883c741002
JdDonovanSb <donovanboni@gmail.com>
2024-09-16 23:03:06 -0500

    feat(back-end): Add Catalogo-related files including controller, model, and views

---
+++ TeddyShop/TeddyShop/Models/Catalogo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace TeddyShop.Models;
+
+[Table("Catalogo")]
+public partial class Catalogo
+{
+    [Key]
+    public int IdCatalogo { get; set; }
+
+    [StringLength(256)]
+    public string DisponibilidadCatalogo { get; set; } = null!;
+
+    [StringLength(256)]
+    public string EstiloCatalogo { get; set; } = null!;
+
+    public int Compañia_NIT { get; set; }
+
+    [ForeignKey("Compañia_NIT")]
+    [InverseProperty("Catalogos")]
+    public virtual Compania Compañia_NITNavigation { get; set; } = null!;
+
+    [ForeignKey("Catalogo_IdCatalogo")]
+    [InverseProperty("Catalogo_IdCatalogos")]
+    public virtual ICollection<Empleado> Empleado_DniEmpleados { get; set; } = new List<Empleado>();
+
+    [ForeignKey("Catalogo_IdCatalogo")]
+    [InverseProperty("Catalogo_IdCatalogos")]
+    public virtual ICollection<Producto> Producto_IdProductos { get; set; } = new List<Producto>();
+}
