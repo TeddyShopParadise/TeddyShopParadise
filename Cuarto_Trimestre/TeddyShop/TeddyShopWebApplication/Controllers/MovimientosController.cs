e0f52e57b9e586a4e35880c747c1d8f44348af86
JordanCastaneda0711 <jordanlozano1123@gmail.com>
2024-09-30 23:17:23 -0500

    feat(auth): Add role-based authentication to Inventarios and Movimientos controllers

--- Cuarto_Trimestre/TeddyShop/TeddyShopWebApplication/Controllers/MovimientosController.cs
+++ Cuarto_Trimestre/TeddyShop/TeddyShopWebApplication/Controllers/MovimientosController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using TeddyShopWebApplication.Models;
 
 namespace TeddyShopWebApplication.Controllers
 {
+    [Authorize(Roles = "Administrador, Empleado, Vendedor")]
     public class MovimientosController : Controller
     {
         private readonly ApplicationDbContext _context;
