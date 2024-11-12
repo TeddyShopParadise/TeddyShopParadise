ff68b989cf74dfa6b231fa16fd39a627acb20cc4
Andres-Espinosa <amdresesp21@gmail.com>
2024-11-12 05:31:17 -0500

    feat (back-end): fix bugs

--- Quinto_Trimestre/TeddyShop - BackEnd/Validations/devoluciones_validation.js
+++ Quinto_Trimestre/TeddyShop - BackEnd/Validations/devoluciones_validation.js
@@ -2,34 +2,6 @@
 const Joi = require('@hapi/joi');
 
 const devolucionesSchemaValidation = Joi.object({
-    numDevolucion: Joi.number()
-        .required()
-        .messages({
-            'number.base': 'El número de devolución debe ser un número',
-            'any.required': 'El número de devolución es un campo requerido'
-        }),
-    motivoDevolucion: Joi.string()
-        .required()
-        .messages({
-            'string.base': 'El motivo de la devolución debe ser un texto',
-            'any.required': 'El motivo de la devolución es un campo requerido'
-        }),
-    fechaDevolucion: Joi.date()
-        .required()
-        .messages({
-            'date.base': 'La fecha de devolución debe ser una fecha válida',
-            'any.required': 'La fecha de devolución es un campo requerido'
-        }),
-    productoId: Joi.string()
-        .length(24)
-        .hex()
-        .required()
-        .messages({
-            'string.base': 'El ID del producto debe ser un texto',
-            'string.length': 'El ID del producto debe tener 24 caracteres',
-            'string.hex': 'El ID del producto debe ser un ID válido en formato hexadecimal',
-            'any.required': 'El ID del producto es un campo requerido'
-        }),
     detalleDevolucion: Joi.string()
         .required()
         .messages({
